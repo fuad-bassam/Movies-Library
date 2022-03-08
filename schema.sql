0cea032aff5d46088066de340ef482269bb36056
fuad-bassam <f.man.x99@gmail.com>
2022-03-08 16:52:31 +0300

    edit schema.sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS fuadMovie;
 CREATE TABLE IF NOT EXISTS FuadMovie(
 id SERIAL PRIMARY KEY,
 title VARCHAR(5000),
-release_date INTEGER,
+release_date VARCHAR(200),
 poster_path VARCHAR(5000),
 overview VARCHAR(5000),
 user_comment VARCHAR(5000) 
